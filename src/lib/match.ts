b3f842c42ae5b31baf7c63674a760b6b43649bf2
febufenn-cyber <febufenn@gmail.com>
2026-07-12 00:56:39 +0530

    Preserve feasibility order during diversity

--- src/lib/match.ts
+++ src/lib/match.ts
@@ -1,1 +1,15 @@
-export * from "./match-v3";
+export {
+  buildAliasMap,
+  parseIngredientInput,
+  rankIngredientSuggestions,
+  suggestUnlockIngredients,
+} from "./match-v3";
+export type {
+  IngredientAliasMap,
+  IngredientParseResult,
+  IngredientParseSuggestion,
+  MatchOptions,
+  PantryIngredient,
+  UnlockSuggestion,
+} from "./match-v3";
+export { diversifyMatches, matchRecipes } from "./match-ranked";
